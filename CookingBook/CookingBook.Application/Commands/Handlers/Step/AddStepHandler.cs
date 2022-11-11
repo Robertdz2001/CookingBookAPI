0e5e6bad5fa215d7f92507e975d924be64322b8e
Robertdz2001 <robertdz0@onet.pl>
2022-11-11 15:32:46 +0100

    Add separate directories in commands and handlers directories. Add create,remove,change commands for steps.

---
+++ CookingBook/CookingBook.Application/Commands/Handlers/Step/AddStepHandler.cs
@@ -0,0 +1,33 @@
+﻿using CookingBook.Application.Commands.Step;
+using CookingBook.Application.Exceptions;
+using CookingBook.Domain;
+using CookingBook.Shared.Abstractions.Commands;
+
+namespace CookingBook.Application.Commands.Handlers.Step;
+
+public class AddStepHandler : ICommandHandler<AddStep>
+{
+    private readonly IRecipeRepository _repository;
+
+    public AddStepHandler(IRecipeRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task HandleAsync(AddStep command)
+    {
+        var recipe = await _repository.GetAsync(command.RecipeId);
+
+        if (recipe is null)
+        {
+            throw new RecipeNotFoundException(command.RecipeId);
+        }
+
+        var newStep = new Domain.ValueObjects.Step(command.Name);
+        
+        recipe.AddStep(newStep);
+
+        await _repository.UpdateAsync(recipe);
+
+    }
+}
---
+++ CookingBook/CookingBook.Application/Commands/Step/AddStep.cs
@@ -0,0 +1,5 @@
+﻿using CookingBook.Shared.Abstractions.Commands;
+
+namespace CookingBook.Application.Commands.Step;
+
+public record AddStep(Guid RecipeId, string Name) : ICommand;
