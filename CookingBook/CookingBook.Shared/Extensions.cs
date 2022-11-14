53e8f442f9654a7c72687b82c58d91432ca8d9ea
Robertdz2001 <92019652+Robertdz2001@users.noreply.github.com>
2022-11-14 12:55:36 +0100

    Feature/error handling
    * Add Seeder
    
    * Add error handling middleware. Add Logging decorator for commands.

--- CookingBook/CookingBook.Shared/Extensions.cs
+++ CookingBook/CookingBook.Shared/Extensions.cs
@@ -1,4 +1,6 @@
-﻿using CookingBook.Shared.Services;
+﻿using CookingBook.Shared.Exceptions;
+using CookingBook.Shared.Services;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CookingBook.Shared;
@@ -8,6 +10,13 @@ public static class Extensions
     public static IServiceCollection AddShared(this IServiceCollection services)
     {
         services.AddHostedService<AppInitializer>();
+        services.AddScoped<ExceptionMiddleware>();
         return services;
+    }
+
+    public static IApplicationBuilder UseShared(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<ExceptionMiddleware>();
+        return app;
     }
 }
