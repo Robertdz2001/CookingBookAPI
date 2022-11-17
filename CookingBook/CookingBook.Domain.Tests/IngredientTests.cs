81a75e1c2e76a32defb0463fd48cd3aee4313b15
Robertdz2001 <robertdz0@onet.pl>
2022-11-17 21:18:49 +0100

    Add tests for ingredient.

---
+++ CookingBook/CookingBook.Domain.Tests/IngredientTests.cs
@@ -0,0 +1,46 @@
+﻿using CookingBook.Domain.Exceptions;
+using CookingBook.Domain.ValueObjects;
+using Shouldly;
+
+namespace CookingBook.Domain.Tests;
+
+public class IngredientTests
+{
+    [Fact]
+    public void
+        IngredientConstructor_Throws_EmptyIngredientNameException_When_Name_Is_Empty()
+    {
+        var exception = Record.Exception(() => new Ingredient("", 321, 321));
+
+        exception.ShouldNotBeNull();
+        exception.ShouldBeOfType<EmptyIngredientNameException>();
+    }
+    
+       
+    [Fact]
+    public void
+        IngredientConstructor_Creates_New_Ingredient_When_Proper_Parameters_Are_Given()
+    {
+        Ingredient ingredient = new Ingredient("Ingredient 321", 321, 321);
+        
+        var exception = Record.Exception(() => ingredient = new Ingredient("Ingredient", 321, 321));
+
+        exception.ShouldBeNull();
+        ingredient.Name.ShouldBe("Ingredient");
+    }
+
+    [Fact]
+    public void
+        GetCalories_Returns_Correct_Value()
+    {
+        Ingredient ingredient = new Ingredient("Ingredient", 75, 300);
+
+        var result = ingredient.getCalories();
+        
+        
+        result.ShouldBe(225);
+
+
+
+    }
+}
--- CookingBook/CookingBook.Domain.Tests/UserTests.cs
+++ CookingBook/CookingBook.Domain.Tests/UserTests.cs
@@ -29,7 +29,7 @@ public class UserTests
     
     [Fact]
     public void
-        UserConstructor_Throws_EmptyPasswordHashException_When_UserName_Is_Empty()
+        UserConstructor_Throws_EmptyPasswordHashException_When_PasswordHash_Is_Empty()
     {
         var exception = Record.Exception(() => new User(Guid.NewGuid(), "UserName", ""));
 
@@ -47,6 +47,6 @@ public class UserTests
         var exception = Record.Exception(() => user = new User(Guid.NewGuid(), "UserName", "PasswordHash"));
 
         exception.ShouldBeNull();
-        user.PasswordHash.ShouldBeEquivalentTo((PasswordHash)"PasswordHash");
+        user.PasswordHash.ShouldBe((PasswordHash)"PasswordHash");
     }
 }
