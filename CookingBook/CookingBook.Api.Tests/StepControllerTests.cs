fc4944c5bcf68328cdee02307642d9296fbc05b8
Robertdz2001 <robertdz0@onet.pl>
2022-12-03 22:39:49 +0100

    Add tests for step and tool controllers.

---
+++ CookingBook/CookingBook.Api.Tests/StepControllerTests.cs
@@ -0,0 +1,285 @@
+﻿using System.Net;
+using System.Text;
+using CookingBook.Api.Models;
+using CookingBook.Domain.Entities;
+using CookingBook.Domain.ValueObjects;
+using CookingBook.Infrastructure.EF.Contexts;
+using Microsoft.AspNetCore.Authorization.Policy;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Shouldly;
+
+namespace CookingBook.Api.Tests;
+
+public class StepControllerTests: IClassFixture<WebApplicationFactory<Program>>
+{
+    #region POST_TESTS
+
+    [Fact]
+    public async Task
+        Post_Returns_NotFound_When_There_Is_No_Recipe_With_Given_Id()
+    {
+        var model = new AddStepModel("Name");
+        
+        var json = JsonConvert.SerializeObject(model);
+        
+        var httpContent = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
+        
+        var response = await _client.PostAsync($"api/recipes/{Guid.NewGuid()}/steps", httpContent);
+        
+        response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+        
+    }
+    
+    [Fact]
+    public async Task
+        Post_Returns_Unauthorized_When_User_Is_Not_Author_Or_Admin()
+    {
+        var model = new AddStepModel("Name");
+    
+        var rid = Guid.NewGuid();
+        
+        var recipe = new Recipe(Guid.NewGuid(), rid, "Recipe", "Url", 39, DateTime.UtcNow);
+        
+        await _writeDbContext.AddAsync(recipe);
+        await _writeDbContext.SaveChangesAsync();
+        var json = JsonConvert.SerializeObject(model);
+        
+        var httpContent = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
+        
+        var response = await _client.PostAsync($"api/recipes/{rid}/steps", httpContent);
+        
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+        
+    }
+    
+    [Fact]
+    public async Task
+        Post_Returns_Created_On_Success()
+    {
+        var model = new AddStepModel("Name");
+    
+        var rid = Guid.NewGuid();
+        
+        var recipe = new Recipe(Guid.Parse("bb21ce33-ea66-4c56-aefc-5f8588f95766"), rid, "Recipe", "Url", 39, DateTime.UtcNow);
+        
+        await _writeDbContext.AddAsync(recipe);
+        await _writeDbContext.SaveChangesAsync();
+        var json = JsonConvert.SerializeObject(model);
+        
+        var httpContent = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
+        
+        var response = await _client.PostAsync($"api/recipes/{rid}/steps", httpContent);
+        
+        response.StatusCode.ShouldBe(HttpStatusCode.Created);
+        response.Headers.Location.ShouldNotBeNull();
+    
+    }
+    #endregion
+    
+    #region PUT_TESTS
+    
+    [Fact]
+    public async Task
+        Put_Returns_Ok_On_Success()
+    {
+        var model = new AddToolModel("Name", 30);
+        
+        var recipe =
+            GetUsersRecipeWithStep(Guid.Parse("bb21ce33-ea66-4c56-aefc-5f8588f95766"), "StepToChange");
+        
+        await _writeDbContext.AddAsync(recipe);
+        await _writeDbContext.SaveChangesAsync();
+        
+        var json = JsonConvert.SerializeObject(model);
+        
+        var httpContent = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
+        
+        var response = await _client.PutAsync($"api/recipes/{(Guid)recipe.Id}/steps/StepToChange", httpContent);
+        
+        response.StatusCode.ShouldBe(HttpStatusCode.OK);
+        
+    }
+    
+    [Fact]
+    public async Task
+        Put_Returns_NotFound_When_There_Is_No_Recipe_With_Given_Id()
+    {
+        var model = new AddStepModel("Name");
+    
+        var json = JsonConvert.SerializeObject(model);
+        
+        var httpContent = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
+        
+        var response = await _client.PutAsync($"api/recipes/{Guid.NewGuid()}/steps/name", httpContent);
+        
+        response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+        
+    }
+    
+    [Fact]
+    public async Task
+        Put_Returns_NotFound_When_There_Is_No_Step_With_Given_Name()
+    {
+        var model = new AddStepModel("Name");
+        
+        var recipe =
+            GetUsersRecipeWithStep(Guid.Parse("bb21ce33-ea66-4c56-aefc-5f8588f95766"), "StepToChange");
+        
+        await _writeDbContext.AddAsync(recipe);
+        await _writeDbContext.SaveChangesAsync();
+    
+        var json = JsonConvert.SerializeObject(model);
+        
+        var httpContent = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
+        
+        var response = await _client.PutAsync($"api/recipes/{(Guid)recipe.Id}/steps/name", httpContent);
+        
+        response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+        
+    }
+    
+    [Fact]
+    public async Task
+        Put_Returns_Unauthorized_When_User_Is_Not_Author_Or_Admin()
+    {
+        var model = new AddStepModel("Name");
+        
+        var recipe = GetUsersRecipeWithStep(Guid.NewGuid(),"StepToChange");
+        
+        await _writeDbContext.AddAsync(recipe);
+        await _writeDbContext.SaveChangesAsync();
+    
+        var json = JsonConvert.SerializeObject(model);
+        
+        var httpContent = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
+        
+        var response = await _client.PutAsync($"api/recipes/{(Guid)recipe.Id}/steps/StepToChange", httpContent);
+        
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+        
+    }
+     #endregion
+    
+    #region DELETE_TESTS
+    
+    [Fact]
+    public async Task
+        Delete_Returns_NoContent_OnSuccess()
+    {
+        var recipe = GetUsersRecipeWithStep(Guid.Parse("bb21ce33-ea66-4c56-aefc-5f8588f95766"),"StepToDelete");
+        
+        await _writeDbContext.AddAsync(recipe);
+        await _writeDbContext.SaveChangesAsync();
+    
+        var response = await _client.DeleteAsync($"api/recipes/{(Guid)recipe.Id}/steps/StepToDelete");
+        
+        response.StatusCode.ShouldBe((HttpStatusCode.NoContent));
+    
+    
+    }
+    
+    
+    [Fact]
+    public async Task
+        Delete_Returns_NotFound_When_There_Is_No_Recipe_With_Given_Id()
+    {
+    
+        var response = await _client.DeleteAsync($"api/recipes/{Guid.NewGuid()}/steps/StepToDelete");
+    
+        response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+    
+    }
+    
+    [Fact]
+    public async Task
+        Delete_Returns_NotFound_When_There_Is_No_Step_With_Given_Name()
+    {
+    
+        var recipe = GetUsersRecipeWithStep(Guid.Parse("bb21ce33-ea66-4c56-aefc-5f8588f95766"), "Name");
+        
+        await _writeDbContext.AddAsync(recipe);
+        await _writeDbContext.SaveChangesAsync();
+        
+        var response = await _client.DeleteAsync($"api/recipes/{(Guid)recipe.Id}/steps/StepToDelete");
+    
+        response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+    
+    }
+    
+    [Fact]
+    public async Task
+        Delete_Returns_Unauthorized_When_User_Is_Not_Author_Or_Admin()
+    {
+        var recipe = GetUsersRecipeWithStep(Guid.NewGuid(), "StepToDelete");
+        
+        await _writeDbContext.AddAsync(recipe);
+        await _writeDbContext.SaveChangesAsync();
+        
+        var response = await _client.DeleteAsync($"api/recipes/{(Guid)recipe.Id}/steps/StepToDelete");
+    
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+        
+        
+    }
+    #endregion
+    
+    #region ARRANGE
+    private HttpClient _client;
+    private WebApplicationFactory<Program> _factory;
+    private ReadDbContext _readDbContext;
+    private WriteDbContext _writeDbContext;
+    
+    
+    private Recipe GetUsersRecipeWithStep(Guid userId, string stepName)
+    {
+    
+        var recipe = new Recipe(userId, Guid.NewGuid(), "Name", "Url", 30,
+            DateTime.UtcNow);
+    
+        var step = new Step(stepName);
+        
+        recipe.AddStep(step);
+    
+        return recipe;
+    }
+    
+    public StepControllerTests(WebApplicationFactory<Program> factory)
+    {
+        
+        _factory = factory.WithWebHostBuilder(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                var readDbContextOptions = services
+                    .SingleOrDefault(service => service.ServiceType == typeof(DbContextOptions<ReadDbContext>));
+                
+                var writeDbContextOptions = services
+                    .SingleOrDefault(service => service.ServiceType == typeof(DbContextOptions<WriteDbContext>));
+    
+                services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>();
+                services.AddMvc(option => option.Filters.Add(new FakeUserFilter()));
+                
+                services.Remove(readDbContextOptions);
+                services.Remove(writeDbContextOptions);
+                services.AddDbContext<ReadDbContext>(options => options.UseInMemoryDatabase("ReadDb"));
+                services.AddDbContext<WriteDbContext>(options => options.UseInMemoryDatabase("WriteDb"));
+            });
+        });
+    
+        _client = _factory.CreateClient();
+        
+        
+        var scopeFactory = _factory.Services.GetService<IServiceScopeFactory>();
+    
+        var scope = scopeFactory.CreateScope();
+    
+        _readDbContext = scope.ServiceProvider.GetService<ReadDbContext>();
+        _writeDbContext = scope.ServiceProvider.GetService<WriteDbContext>();
+    }
+    
+    
+    #endregion
+}
