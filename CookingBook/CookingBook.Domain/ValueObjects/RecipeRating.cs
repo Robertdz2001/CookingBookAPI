fba9bfb5309b6dab96507dd1c7b380ffdd703525
Robertdz2001 <robertdz0@onet.pl>
2022-12-19 15:25:25 +0100

    Add RecipeRating private field for Recipe.

---
+++ CookingBook/CookingBook.Domain/ValueObjects/RecipeRating.cs
@@ -0,0 +1,8 @@
+﻿namespace CookingBook.Domain.ValueObjects;
+
+public record RecipeRating(short Value)
+{
+    public static implicit operator short(RecipeRating rating) => rating.Value;
+    
+    public static implicit operator RecipeRating(short value) => new(value);
+}
