15babea61216a0ad979b29809bd374827332f1e6
Robertdz2001 <robertdz0@onet.pl>
2022-12-19 15:42:51 +0100

    Add tests for reviews constructor.

---
+++ CookingBook/CookingBook.Domain.Tests/ReviewTests.cs
@@ -0,0 +1,39 @@
+﻿using CookingBook.Domain.Exceptions;
+using CookingBook.Domain.ValueObjects;
+using Shouldly;
+
+namespace CookingBook.Domain.Tests;
+
+public class ReviewTests
+{
+    [Fact]
+    public void ReviewConstructor_Throws_EmptyReviewNameException_When_Name_Is_Empty()
+    {
+        var exception = Record.Exception(() => new Review("", "Content", 5));
+
+        exception.ShouldNotBeNull();
+        exception.ShouldBeOfType<EmptyReviewNameException>();
+    }
+    
+    [Fact]
+    public void ReviewConstructor_Throws_EmptyReviewContentException_When_Name_Is_Empty()
+    {
+        var exception = Record.Exception(() => new Review("Review", "", 5));
+
+        exception.ShouldNotBeNull();
+        exception.ShouldBeOfType<EmptyReviewContentException>();
+    }
+    
+    [Theory]
+    [InlineData(-6)]
+    [InlineData(-12345)]
+    [InlineData(6)]
+    [InlineData(100)]
+    public void ReviewConstructor_Throws_InvalidReviewRateException_When_Rate_Is_Not_Between_Minus_5_And_5(short rate)
+    {
+        var exception = Record.Exception(() => new Review("Review", "Content", rate));
+
+        exception.ShouldNotBeNull();
+        exception.ShouldBeOfType<InvalidReviewRateException>();
+    }
+}
