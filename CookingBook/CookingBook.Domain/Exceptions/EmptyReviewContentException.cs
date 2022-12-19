191aa8807ea8c120cea74d6751ca3d5c64470300
Robertdz2001 <robertdz0@onet.pl>
2022-12-19 14:12:41 +0100

    Add Review value object.

---
+++ CookingBook/CookingBook.Domain/Exceptions/EmptyReviewContentException.cs
@@ -0,0 +1,10 @@
+﻿using CookingBook.Shared.Abstractions.Exceptions;
+
+namespace CookingBook.Domain.Exceptions;
+
+public class EmptyReviewContentException : CookingBookBadRequestException
+{
+    public EmptyReviewContentException() : base("Review content cannot be empty.")
+    {
+    }
+}
