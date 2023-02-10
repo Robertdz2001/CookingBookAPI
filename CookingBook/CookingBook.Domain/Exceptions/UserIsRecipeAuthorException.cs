b849eb63dd06fadd43719edd927e1c5c1b6b7d9d
Robertdz2001 <robertdz0@onet.pl>
2023-02-10 23:36:16 +0100

    Add check if user adding a review is not a recipe author.

---
+++ CookingBook/CookingBook.Domain/Exceptions/UserIsRecipeAuthorException.cs
@@ -0,0 +1,10 @@
+﻿using CookingBook.Shared.Abstractions.Exceptions;
+
+namespace CookingBook.Domain.Exceptions;
+
+public class UserIsRecipeAuthorException : CookingBookBadRequestException
+{
+    public UserIsRecipeAuthorException() : base("Author cannot add reviews to his recipes.")
+    {
+    }
+}
